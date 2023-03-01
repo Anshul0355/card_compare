c18a13a67eb534023dff7f987007f02a76402846
ansh <anshul.thoughtwin@gmail.com>
2023-03-01 17:45:04 +0530

    all setup done

---
+++ card_compare/apps/card_test/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+# # from django.views.generic import TemplateView
+# from django.contrib.auth import views as auth_views
+
+
+
+app_name = 'card_test'  
+urlpatterns = [
+]
